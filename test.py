7a309d126412b6ef98c4fb7016da0a1b8e45b0aa
SYJINTW <52921547+SYJINTW@users.noreply.github.com>
2022-05-02 23:07:16 +0800

    add arg parser

--- test.py
+++ test.py
@@ -4,11 +4,14 @@ import pandas as pd
 import time
 import signal
 import sys
+import argparse
 
 results = []
+save_filename = 'test'
+
 def signal_handler(sig, frame):
     print('You pressed Ctrl+C!')
-    pd.DataFrame(results).to_csv('results.csv', header=['time','cpu','gpu','mem'],index=None)
+    pd.DataFrame(results).to_csv(f'{save_filename}.csv', header=['time','cpu','gpu','mem'],index=None)
     sys.exit(0)
 
 def main():
@@ -23,8 +26,12 @@ def main():
         gpu_load = GPUs[0].load
         mem_load = mem.used/total_mem
         results.append([current_time,cpu_load,gpu_load,mem_load])
-        print(f'TIME: {current_time}, CPU: {cpu_load}, GPU: {gpu_load}, MEM: {mem_load}')
+        # print(f'TIME: {current_time}, CPU: {cpu_load}, GPU: {gpu_load}, MEM: {mem_load}')
     
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f','--filename',help='filename',type=str)
+    args = parser.parse_args()
+    save_filename = args.filename
     signal.signal(signal.SIGINT, signal_handler)
     main()
